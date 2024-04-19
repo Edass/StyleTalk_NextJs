fcebde7f7dc418423737d5b280ed00148ff38c64
Eda Sarıkaya <edassarkaya@outlook.com>
2024-04-19 17:35:22 +0100

    added

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductLayout = ({children}) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default ProductLayout
---
+++ src/app/(main)/product/page.jsx
@@ -0,0 +1,13 @@
+
+import CardList from '@/component/products/CardList'
+import React from 'react'
+
+const ProductPage = () => {
+  return (
+    <div>
+      <CardList/>
+    </div>
+  )
+}
+
+export default ProductPage
